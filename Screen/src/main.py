3642e549eaaca1fef1cf42c745580f9e48a2e388
Gibi500 <axel_broeckaert@hotmail.be>
2024-08-20 14:45:52 +0200

    making sure it breaks correctly

--- Screen/src/main.py
+++ Screen/src/main.py
@@ -74,6 +74,7 @@ def detect_touch(e_rd, e_rdw):
                 print("page= {}, component= {}, event= {}".format(pageID_touch,compID_touch,event_touch))
 
                 for i in range(0, NUMBER_OF_DEVICES):
+                    print(i)
                     if (pageID_touch, compID_touch) == nxApp.ID_HOME_BUTTON_TOGGLE_DEVICES[i]:
                         toggle_device(pageID_touch, compID_touch, i)
                         print("HOME toggle device")
