2b58d8018872ecb84b2e89712cb61e5c8dcce0ea
Gibi500 <axel_broeckaert@hotmail.be>
2024-08-20 14:41:45 +0200

    more debugging

--- Screen/src/main.py
+++ Screen/src/main.py
@@ -78,10 +78,10 @@ def detect_touch(e_rd, e_rdw):
                 for i in range(0, NUMBER_OF_DEVICES):
                     print(nxApp.ID_BUTTON_TOGGLE_DEVICES[i])
                     if (pageID_touch, compID_touch) == nxApp.ID_HOME_BUTTON_TOGGLE_DEVICES[i]:
-                        toggle_device(i)
+                        toggle_device(pageID_touch, compID_touch, i)
                         print("HOME toggle device")
                     if (pageID_touch, compID_touch) == nxApp.ID_BUTTON_TOGGLE_DEVICES[i]:
-                        toggle_device(i)
+                        toggle_device(pageID_touch, compID_touch, i)
                         print("toggle device")
         except:
             pass
