8d467a01b55d794c7633027e1da989b397ce4292
Gibi500 <axel_broeckaert@hotmail.be>
2024-09-02 15:12:03 +0200

    screen debugging

--- Screen/src/nextionApp.py
+++ Screen/src/nextionApp.py
@@ -82,7 +82,48 @@ class NextionApp:
                     {'id': 22, 'type': 'text', 'name': 't18'}, # Active Power
                     {'id': 23, 'type': 'text', 'name': 't19'} # Imaginary Power
              ]
-             }
+             },
+            {'id': 6, 'name': 'Device 6',
+                'components': [
+                    {'id': 2, 'type': 'button', 'name': 'b0'}, # Toggle Button
+                    {'id': 3, 'type': 'button', 'name': 'b1'}, # Stats refresh Button
+                    {'id': 17, 'type': 'text', 'name': 't13'}, # Voltage
+                    {'id': 18, 'type': 'text', 'name': 't14'}, # Current
+                    {'id': 19, 'type': 'text', 'name': 't15'}, # Peak Current
+                    {'id': 20, 'type': 'text', 'name': 't16'}, # Power factor
+                    {'id': 21, 'type': 'text', 'name': 't17'}, # Apparent Power
+                    {'id': 22, 'type': 'text', 'name': 't18'}, # Active Power
+                    {'id': 23, 'type': 'text', 'name': 't19'} # Imaginary Power
+             ]
+             },
+            {'id': 7, 'name': 'Device 7',
+                'components': [
+                    {'id': 2, 'type': 'button', 'name': 'b0'}, # Toggle Button
+                    {'id': 3, 'type': 'button', 'name': 'b1'}, # Stats refresh Button
+                    {'id': 17, 'type': 'text', 'name': 't13'}, # Voltage
+                    {'id': 18, 'type': 'text', 'name': 't14'}, # Current
+                    {'id': 19, 'type': 'text', 'name': 't15'}, # Peak Current
+                    {'id': 20, 'type': 'text', 'name': 't16'}, # Power factor
+                    {'id': 21, 'type': 'text', 'name': 't17'}, # Apparent Power
+                    {'id': 22, 'type': 'text', 'name': 't18'}, # Active Power
+                    {'id': 23, 'type': 'text', 'name': 't19'} # Imaginary Power
+             ]
+             },
+            {'id': 8, 'name': 'Device 8',
+                'components': [
+                    {'id': 2, 'type': 'button', 'name': 'b0'}, # Toggle Button
+                    {'id': 3, 'type': 'button', 'name': 'b1'}, # Stats refresh Button
+                    {'id': 17, 'type': 'text', 'name': 't13'}, # Voltage
+                    {'id': 18, 'type': 'text', 'name': 't14'}, # Current
+                    {'id': 19, 'type': 'text', 'name': 't15'}, # Peak Current
+                    {'id': 20, 'type': 'text', 'name': 't16'}, # Power factor
+                    {'id': 21, 'type': 'text', 'name': 't17'}, # Apparent Power
+                    {'id': 22, 'type': 'text', 'name': 't18'}, # Active Power
+                    {'id': 23, 'type': 'text', 'name': 't19'} # Imaginary Power
+                ]
+            }
+
+
 
         ]
 
@@ -104,6 +145,9 @@ ID_HOME_BUTTON_TOGGLE_DEVICE2 = get_Ids('home', 't1')
 ID_HOME_BUTTON_TOGGLE_DEVICE3 = get_Ids('home', 't2')
 ID_HOME_BUTTON_TOGGLE_DEVICE4 = get_Ids('home', 't3')
 ID_HOME_BUTTON_TOGGLE_DEVICE5 = get_Ids('home', 't4')
+ID_HOME_BUTTON_TOGGLE_DEVICE6 = get_Ids('home', 't5')
+ID_HOME_BUTTON_TOGGLE_DEVICE7 = get_Ids('home', 't6')
+ID_HOME_BUTTON_TOGGLE_DEVICE8 = get_Ids('home', 't7')
 
 # Device 1 page
 ID_BUTTON_TOGGLE_DEVICE1 = get_Ids('Device 1', 'b0')
@@ -160,6 +204,39 @@ ID_DEVICE5_APPARENT_POWER = get_Ids('Device 5', 't17')
 ID_DEVICE5_ACTIVE_POWER = get_Ids('Device 5', 't18')
 ID_DEVICE5_IMAGINARY_POWER = get_Ids('Device 5', 't19')
 
+# Device 6 page
+ID_BUTTON_TOGGLE_DEVICE6 = get_Ids('Device 6', 'b0')
+ID_DEVICE6_REFRESH = get_Ids('Device 6', 'b1')
+ID_DEVICE6_VOLTAGE = get_Ids('Device 6', 't13')
+ID_DEVICE6_CURRENT = get_Ids('Device 6', 't14')
+ID_DEVICE6_PEAK_CURRENT = get_Ids('Device 6', 't15')
+ID_DEVICE6_POWER_FACTOR = get_Ids('Device 6', 't16')
+ID_DEVICE6_APPARENT_POWER = get_Ids('Device 6', 't17')
+ID_DEVICE6_ACTIVE_POWER = get_Ids('Device 6', 't18')
+ID_DEVICE6_IMAGINARY_POWER = get_Ids('Device 6', 't19')
+
+# Device 7 page
+ID_BUTTON_TOGGLE_DEVICE7 = get_Ids('Device 7', 'b0')
+ID_DEVICE7_REFRESH = get_Ids('Device 7', 'b1')
+ID_DEVICE7_VOLTAGE = get_Ids('Device 7', 't13')
+ID_DEVICE7_CURRENT = get_Ids('Device 7', 't14')
+ID_DEVICE7_PEAK_CURRENT = get_Ids('Device 7', 't15')
+ID_DEVICE7_POWER_FACTOR = get_Ids('Device 7', 't16')
+ID_DEVICE7_APPARENT_POWER = get_Ids('Device 7', 't17')
+ID_DEVICE7_ACTIVE_POWER = get_Ids('Device 7', 't18')
+ID_DEVICE7_IMAGINARY_POWER = get_Ids('Device 7', 't19')
+
+# Device 8 page
+ID_BUTTON_TOGGLE_DEVICE8 = get_Ids('Device 8', 'b0')
+ID_DEVICE8_REFRESH = get_Ids('Device 8', 'b1')
+ID_DEVICE8_VOLTAGE = get_Ids('Device 8', 't13')
+ID_DEVICE8_CURRENT = get_Ids('Device 8', 't14')
+ID_DEVICE8_PEAK_CURRENT = get_Ids('Device 8', 't15')
+ID_DEVICE8_POWER_FACTOR = get_Ids('Device 8', 't16')
+ID_DEVICE8_APPARENT_POWER = get_Ids('Device 8', 't17')
+ID_DEVICE8_ACTIVE_POWER = get_Ids('Device 8', 't18')
+ID_DEVICE8_IMAGINARY_POWER = get_Ids('Device 8', 't19')
+
 #lookup tables
-ID_HOME_BUTTON_TOGGLE_DEVICES = [ID_HOME_BUTTON_TOGGLE_DEVICE1, ID_HOME_BUTTON_TOGGLE_DEVICE2, ID_HOME_BUTTON_TOGGLE_DEVICE3, ID_HOME_BUTTON_TOGGLE_DEVICE4, ID_HOME_BUTTON_TOGGLE_DEVICE5]
-ID_BUTTON_TOGGLE_DEVICES = [ID_BUTTON_TOGGLE_DEVICE1, ID_BUTTON_TOGGLE_DEVICE2, ID_BUTTON_TOGGLE_DEVICE3, ID_BUTTON_TOGGLE_DEVICE4, ID_BUTTON_TOGGLE_DEVICE5]
+ID_HOME_BUTTON_TOGGLE_DEVICES = [ID_HOME_BUTTON_TOGGLE_DEVICE1, ID_HOME_BUTTON_TOGGLE_DEVICE2, ID_HOME_BUTTON_TOGGLE_DEVICE3, ID_HOME_BUTTON_TOGGLE_DEVICE4, ID_HOME_BUTTON_TOGGLE_DEVICE5, ID_HOME_BUTTON_TOGGLE_DEVICE6, ID_HOME_BUTTON_TOGGLE_DEVICE7, ID_HOME_BUTTON_TOGGLE_DEVICE8]
+ID_BUTTON_TOGGLE_DEVICES = [ID_BUTTON_TOGGLE_DEVICE1, ID_BUTTON_TOGGLE_DEVICE2, ID_BUTTON_TOGGLE_DEVICE3, ID_BUTTON_TOGGLE_DEVICE4, ID_BUTTON_TOGGLE_DEVICE5, ID_BUTTON_TOGGLE_DEVICE6, ID_BUTTON_TOGGLE_DEVICE7, ID_BUTTON_TOGGLE_DEVICE8]
